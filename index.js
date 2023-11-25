0d68724cba3e4837d5c0493db0a5534da9d5bdf1
sohan0898 <shuhanur.rahman08@gmail.com>
2023-11-26 00:48:22 +0600

    by post method constests are added to Db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const ports = process.env.PORT || 5000;
@@ -11,7 +12,7 @@ app.use(express.json());
 
 //mongoDB
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8p2aqm7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,22 +21,56 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const userCollection = client.db("ContestDB").collection("users");
+    const contestCollection = client.db("ContestDB").collection("contests");
 
 
     //routes
-    const UserCollection = client.db('ContestDB').collection('user');
+  
+
+    
+
+    //contests api
+    app.post('/contests', async (req, res) => {
+        const item = req.body;
+        const result = await contestCollection.insertOne(item);
+        res.send(result);
+      });
+
+    //user api
+
+    app.get('/users',  async (req, res) => {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      });
+
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
 
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -43,36 +78,31 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-res.send('Contest-Hub Server Started');
+app.get("/", (req, res) => {
+  res.send("Contest-Hub Server Started");
 });
-
 
 // check server api health
 
 app.get("/health", (req, res) => {
-    res.send("Contest-Hub is running....");
+  res.send("Contest-Hub is running....");
+});
+
+//error Handle for post, get
+
+app.all("*", (req, res, next) => {
+  const error = new Error(`Can't find ${req.originalUrl} on the server`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
   });
-
-
-  //error Handle for post, get
-
-  app.all("*", (req, res, next) => {
-    const error = new Error(`Can't find ${req.originalUrl} on the server`);
-    error.status = 404;
-    next(error);
-  });
-
-  
-  app.use((err, req,res,next)=>{
-    res.status(err.status || 500).json({
-        message: err.message,
-        errors: err.errors,
-      });
-  })
-
-
+});
 
 app.listen(ports, () => {
-console.log( `server running on port ${ports}`);
+  console.log(`server running on port ${ports}`);
 });
